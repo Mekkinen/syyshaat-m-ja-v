ebae1070f4bbc2912633c7a0db2a800583969d53
Mea <mea.ekroos@helsinki.fi>
2025-07-30 15:26:41 +0300

    add more info

--- src/App.js
+++ src/App.js
@@ -6,27 +6,32 @@ export function App() {
       <Header />
       <div className="surroundBox">
         <div className="infobox centerPadding">
-          <p><a href="">RSVP</a></p>
-          <p><i>Vastaathan 10.8.2025 mennessä yllä olevan RSVP-linkin kautta</i></p>
+          <h2>RSVP</h2>
+          <p><i>Ilmoitathan tulostasi 10.8.2025 mennessä alla olevan RSVP-linkin kautta</i></p>
+          <p>Samassa lomakkeessa ilmoitathan allergiat ja muut ruokavaliorajoitteet. Ruoka on kokonaan vegaanista, joten kasvisruokavaliota ei tarvitse erikseen ilmoittaa. Tervetuloa!</p>
+          <p><a href="">RSVP - vastaa tästä</a></p>
         </div>
         <div className="infobox centerPadding">
           <div className="inlineLeft">
-            <div style={{textAlign: "center"}}><p>Aikataulu</p></div>
+            <div style={{textAlign: "center"}}><h2>Aikataulu</h2></div>
             <ul>
               <li>14:45 - Paikalle voi saapua, alkumaljat</li>
               <li>15:30 - Vihkiminen</li>
-              <li>16:15 - Päivällinen</li>
-              <li>18:00 - Jotain</li>
+              <li>16:15 - Päivällinen alkaa</li>
+              <li>17:10 - Kuvia hääparin kanssa</li>
               <li>19:00 - Kakun leikkaus</li>
-              <li>20:00 - DJ soittelee, vapaata hengailua</li>
+              <li>20:00 - DJ soittaa, vapaata hengailua</li>
+              <li>21:55 - Loppuseremonia</li>
             </ul>
           </div>
         </div>
         <div className="infobox centerPadding">
-          <p>Pukukoodi</p>
+          <h2>Pukukoodi</h2>
+          <p>Pukukoodina on juhlapukeutuminen, mutta älä turhaan ota stressiä siitä, oletko tarpeeksi juhlava tai ylipukeutunut.</p>
         </div>
         <div className="infobox centerPadding">
-          <p>Häälahjat</p>
+          <h2>Häälahjat</h2>
+          <p>Mikäli haluat lahjoa meitä ja taloudellinen tilanteesi sen sallii, toivomme lahjoitusta häämatkakassaan! Tilinumeromme on <i>FI18 3636 3010 4953 98</i>, saaja <i>Mea Ekroos</i>.</p>
         </div>
       </div>
     </>
